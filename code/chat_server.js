29866a91bef5f88dc26ecd4b7357677099686327
Mike Cantelon <mcantelon@gmail.com>
2012-12-29 12:16:50 -0800

    More work on presentation.

--- code/chat_server.js
+++ code/chat_server.js
@@ -2,7 +2,7 @@ var net = require('net')
   , clients = {};
 
 var server = net.createServer(function(conn) {
-  var id = conn.remotePort;
+  var id = conn.remoteAddress + ':' + conn.remotePort;
   conn.on('connect', function() {
     clients[id] = conn;
   });
@@ -17,12 +17,12 @@ var server = net.createServer(function(conn) {
 server.listen(3000);
 
 function broadcast(fromId, message) {
-  // if a message, not a control key, has been sent, broadcast it
+  // if a message, not a control key, have been sent, broadcast it
   if (message.toString().slice(-1) == "\n") {
     // send to each client except sender
     for(var id in clients) {
       if (fromId != id) {
-        clients[id].write(fromId + '> ' + message);
+        clients[id].write(id + '> ' + message);
       }
     }
   }
