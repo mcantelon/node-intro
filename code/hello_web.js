2940026d4d200b4d8e3eeb737739d995f3a2424a
Mike Cantelon <mcantelon@gmail.com>
2014-01-18 20:44:03 -0800

    Removed needed directories.

--- code/hello_web.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-
-http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello Web!\n');
-}).listen(8888);
